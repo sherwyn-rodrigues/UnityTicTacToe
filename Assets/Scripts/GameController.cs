82fb6f0e35263844ba1430e000ea92fa425bad7b
sherwyn47 <rodriguessherwyn47@gmail.com>
2025-04-15 13:43:48 +0530

    2d serialisable array

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,26 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using static GridSpace;
 
+
+/// <summary>
+/// this class is for creating a row of 3 elemts since 2d arrays cant be serialised in inspector
+/// </summary>
+
+[System.Serializable]
+public class GridRow
+{
+    public GridSpace[] gridSpaces = new GridSpace[3]; // One row of 3 buttons
+}
+
+// end button row class 
 public class GameController : MonoBehaviour
 {
-    public GridSpace[] GridList;
+    //public GridSpace[,] GridList = new GridSpace[3,3];
+    public GridRow[] uiGrid = new GridRow[3];
+
     private ButtonState PlayerSide;
 
     private void Awake()
@@ -16,9 +31,13 @@ public class GameController : MonoBehaviour
 
     void SetGameControlelrReferenceButtons()
     {
-        for(int i = 0; i < GridList.Length; i++)
+        for(int row = 0; row < 3; row++)
         {
-            GridList[i].SetGameControllerReference(this);
+            for(int col = 0; col < 3; col++)
+            {
+                GridSpace tempGridSpace = uiGrid[row].gridSpaces[col];
+                tempGridSpace.SetGameControllerReference(this);
+            }
         }
     }
 
